d765b5cf06048308258ecbf50c8a692c2993040b
programacaoemacao <programacaoemacaobr@gmail.com>
2023-12-13 22:02:34 -0300

    Showing the app in fullscreen mode

--- main.go
+++ main.go
@@ -232,7 +232,7 @@ func (m mainModel) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(newModel())
+	p := tea.NewProgram(newModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
